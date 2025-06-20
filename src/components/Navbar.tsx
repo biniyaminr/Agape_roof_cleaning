c8c75ae1540bda06775aa8df5a9558407f44642b
Biniyam <benjamindereje12@gmail.com>
2025-06-20 19:53:02 +0300

    Embed phone number and email as links in paragraphs with Source Sans 3 font in Contact section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -136,9 +136,9 @@ const Navbar = () => {
           {/* Logo Section */}
           <Link to="/" className="flex items-center">
             <img
-              src="https://i.ibb.co/rRwtDybC/New-Project.png"
+              src="https://i.ibb.co/PZcpzDB0/agape-main.png"
               alt="Agape Roof Cleaning Logo"
-              className="h-14 w-14 mr-3 rounded-full"
+              className="h-30 w-40 mr-3 rounded-full"
             />
             <span className="text-3xl font-semibold text-[#1F2A44] font-bankBold">
               Agape Roof Cleaning
